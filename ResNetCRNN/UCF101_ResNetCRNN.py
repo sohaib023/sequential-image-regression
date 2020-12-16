3941ba4a84705f004d5debe48819ebfae070a447
Sohaib Zahid <sohaib023@gmail.com>
2020-12-16 22:25:27 +0500

    fix bug in Encoder + change dataloaders for personal data

--- ResNetCRNN/UCF101_ResNetCRNN.py
+++ ResNetCRNN/UCF101_ResNetCRNN.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -9,14 +10,16 @@ import torch.utils.data as data
 import torchvision
 from torch.autograd import Variable
 import matplotlib.pyplot as plt
+
 from functions import *
+
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 from sklearn.metrics import accuracy_score
 import pickle
 
 # set path
-data_path = "./jpegs_256/"    # define UCF-101 RGB data path
+data_path = "./data/"    # define UCF-101 RGB data path
 action_name_path = './UCF101actions.pkl'
 save_model_path = "./ResNetCRNN_ckpt/"
 
@@ -32,14 +35,10 @@ RNN_hidden_nodes = 512
 RNN_FC_dim = 256
 
 # training parameters
-k = 101             # number of target category
 epochs = 120        # training epochs
 batch_size = 40  
 learning_rate = 1e-3
 log_interval = 10   # interval for displaying training info
-
-# Select which frame to begin & end in videos
-begin_frame, end_frame, skip_frame = 1, 29, 1
 
 
 def train(log_interval, model, device, train_loader, optimizer, epoch):
@@ -50,31 +49,35 @@ def train(log_interval, model, device, train_loader, optimizer, epoch):
 
     losses = []
     scores = []
-    N_count = 0   # counting total trained sample in one epoch
-    for batch_idx, (X, y) in enumerate(train_loader):
-        # distribute data to device
-        X, y = X.to(device), y.to(device).view(-1, )
 
-        N_count += X.size(0)
+    for batch_idx, (X, y, is_new) in enumerate(train_loader):
+        if is_new[0].item():
+            h_n = c_n = None
+
+        # distribute data to device
+        X, y = X.to(device).squeeze(0), y.to(device).squeeze(0)
 
         optimizer.zero_grad()
-        output = rnn_decoder(cnn_encoder(X))   # output has dim = (batch, number of classes)
 
-        loss = F.cross_entropy(output, y)
-        losses.append(loss.item())
+        output, h_n, c_n = rnn_decoder(cnn_encoder(X), h_0=h_n, c_0=c_n)   # output has dim = (batch, number of classes)
 
-        # to compute accuracy
-        y_pred = torch.max(output, 1)[1]  # y_pred != output
-        step_score = accuracy_score(y.cpu().data.squeeze().numpy(), y_pred.cpu().data.squeeze().numpy())
-        scores.append(step_score)         # computed on CPU
+        output = output.flatten()
+        out_speed = output[1:] - output[:-1]
+
+        pos_loss = F.mse_loss(output, y[:, 0])
+        speed_loss = F.mse_loss(out_speed, y[1:, 1])
+
+        loss = 0.5 * pos_loss + 0.5 * speed_loss
+
+        losses.append((pos_loss.item(), speed_loss.item()))
 
         loss.backward()
         optimizer.step()
 
         # show information
         if (batch_idx + 1) % log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}, Accu: {:.2f}%'.format(
-                epoch + 1, N_count, len(train_loader.dataset), 100. * (batch_idx + 1) / len(train_loader), loss.item(), 100 * step_score))
+            print('Train Epoch: {} [{}]\tPosition Loss: {:.6f}, Speed Loss: {:.6f}'.format(
+                epoch + 1, batch_idx, pos_loss.item(), speed_loss.item()))
 
     return losses, scores
 
@@ -89,26 +92,54 @@ def validation(model, device, optimizer, test_loader):
     all_y = []
     all_y_pred = []
     with torch.no_grad():
-        for X, y in test_loader:
-            # distribute data to device
-            X, y = X.to(device), y.to(device).view(-1, )
 
-            output = rnn_decoder(cnn_encoder(X))
+            output = output.flatten()
+            out_speed = output[1:] - output[:-1]
 
-            loss = F.cross_entropy(output, y, reduction='sum')
-            test_loss += loss.item()                 # sum up batch loss
-            y_pred = output.max(1, keepdim=True)[1]  # (y_pred != output) get the index of the max log-probability
+            pos_loss = F.mse_loss(output, y[:, 0])
+            speed_loss = F.mse_loss(out_speed, y[1:, 1])
+
+            loss = 0.5 * pos_loss + 0.5 * speed_loss
+
+            losses.append((pos_loss.item(), speed_loss.item()))
+
+            loss.backward()
+            optimizer.step()
+
+            # show information
+            if (batch_idx + 1) % log_interval == 0:
+                print('Train Epoch: {} [{}]\tPosition Loss: {:.6f}, Speed Loss: {:.6f}'.format(
+                    epoch + 1, batch_idx, pos_loss.item(), speed_loss.item()))
+
+
+        for batch_idx, (X, y, is_new) in enumerate(test_loader):
+            if is_new[0].item():
+                h_n = c_n = None
+
+            X, y = X.to(device).squeeze(0), y.to(device).squeeze(0)
+
+            output, h_n, c_n = rnn_decoder(cnn_encoder(X), h_0=h_n, c_0=c_n)   # output has dim = (batch, number of classes)
+
+            output = output.flatten()
+            out_speed = output[1:] - output[:-1]
+
+            pos_loss = F.mse_loss(output, y[:, 0])
+            speed_loss = F.mse_loss(out_speed, y[1:, 1])
+
+            loss = 0.5 * pos_loss + 0.5 * speed_loss
+
+            test_loss += loss.item()
 
             # collect all y and y_pred in all batches
-            all_y.extend(y)
-            all_y_pred.extend(y_pred)
+            # all_y.extend(y)
+            # all_y_pred.extend(y_pred)
 
     test_loss /= len(test_loader.dataset)
 
-    # compute accuracy
-    all_y = torch.stack(all_y, dim=0)
-    all_y_pred = torch.stack(all_y_pred, dim=0)
-    test_score = accuracy_score(all_y.cpu().data.squeeze().numpy(), all_y_pred.cpu().data.squeeze().numpy())
+    # # compute accuracy
+    # all_y = torch.stack(all_y, dim=0)
+    # all_y_pred = torch.stack(all_y_pred, dim=0)
+    # test_score = accuracy_score(all_y.cpu().data.squeeze().numpy(), all_y_pred.cpu().data.squeeze().numpy())
 
     # show information
     print('\nTest set ({:d} samples): Average loss: {:.4f}, Accuracy: {:.2f}%\n'.format(len(all_y), test_loss, 100* test_score))
@@ -127,57 +158,17 @@ use_cuda = torch.cuda.is_available()                   # check if GPU exists
 device = torch.device("cuda" if use_cuda else "cpu")   # use CPU or GPU
 
 # Data loading parameters
-params = {'batch_size': batch_size, 'shuffle': True, 'num_workers': 4, 'pin_memory': True} if use_cuda else {}
-
-
-# load UCF101 actions names
-with open(action_name_path, 'rb') as f:
-    action_names = pickle.load(f)
-
-# convert labels -> category
-le = LabelEncoder()
-le.fit(action_names)
-
-# show how many classes there are
-list(le.classes_)
-
-# convert category -> 1-hot
-action_category = le.transform(action_names).reshape(-1, 1)
-enc = OneHotEncoder()
-enc.fit(action_category)
-
-# # example
-# y = ['HorseRace', 'YoYo', 'WalkingWithDog']
-# y_onehot = labels2onehot(enc, le, y)
-# y2 = onehot2labels(le, y_onehot)
-
-actions = []
-fnames = os.listdir(data_path)
-
-all_names = []
-for f in fnames:
-    loc1 = f.find('v_')
-    loc2 = f.find('_g')
-    actions.append(f[(loc1 + 2): loc2])
-
-    all_names.append(f)
-
-
-# list all data files
-all_X_list = all_names                  # all video file names
-all_y_list = labels2cat(le, actions)    # all video labels
+params = {'batch_size': 1}
 
 # train, test split
-train_list, test_list, train_label, test_label = train_test_split(all_X_list, all_y_list, test_size=0.25, random_state=42)
+# train_list, test_list, train_label, test_label = train_test_split(all_X_list, all_y_list, test_size=0.25, random_state=42)
 
 transform = transforms.Compose([transforms.Resize([res_size, res_size]),
                                 transforms.ToTensor(),
                                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
 
-selected_frames = np.arange(begin_frame, end_frame, skip_frame).tolist()
-
-train_set, valid_set = Dataset_CRNN(data_path, train_list, train_label, selected_frames, transform=transform), \
-                       Dataset_CRNN(data_path, test_list, test_label, selected_frames, transform=transform)
+train_set, valid_set = Dataset_CRNN(data_path, transform=transform), \
+                       Dataset_CRNN(data_path, transform=transform)
 
 train_loader = data.DataLoader(train_set, **params)
 valid_loader = data.DataLoader(valid_set, **params)
@@ -186,28 +177,27 @@ valid_loader = data.DataLoader(valid_set, **params)
 # Create model
 cnn_encoder = ResCNNEncoder(fc_hidden1=CNN_fc_hidden1, fc_hidden2=CNN_fc_hidden2, drop_p=dropout_p, CNN_embed_dim=CNN_embed_dim).to(device)
 rnn_decoder = DecoderRNN(CNN_embed_dim=CNN_embed_dim, h_RNN_layers=RNN_hidden_layers, h_RNN=RNN_hidden_nodes, 
-                         h_FC_dim=RNN_FC_dim, drop_p=dropout_p, num_classes=k).to(device)
+                         h_FC_dim=RNN_FC_dim, drop_p=dropout_p).to(device)
 
 # Parallelize model to multiple GPUs
-if torch.cuda.device_count() > 1:
-    print("Using", torch.cuda.device_count(), "GPUs!")
-    cnn_encoder = nn.DataParallel(cnn_encoder)
-    rnn_decoder = nn.DataParallel(rnn_decoder)
+# if torch.cuda.device_count() > 1:
+#     print("Using", torch.cuda.device_count(), "GPUs!")
+#     cnn_encoder = nn.DataParallel(cnn_encoder)
+#     rnn_decoder = nn.DataParallel(rnn_decoder)
 
-    # Combine all EncoderCNN + DecoderRNN parameters
-    crnn_params = list(cnn_encoder.module.fc1.parameters()) + list(cnn_encoder.module.bn1.parameters()) + \
-                  list(cnn_encoder.module.fc2.parameters()) + list(cnn_encoder.module.bn2.parameters()) + \
-                  list(cnn_encoder.module.fc3.parameters()) + list(rnn_decoder.parameters())
+#     # Combine all EncoderCNN + DecoderRNN parameters
+#     crnn_params = list(cnn_encoder.module.fc1.parameters()) + list(cnn_encoder.module.bn1.parameters()) + \
+#                   list(cnn_encoder.module.fc2.parameters()) + list(cnn_encoder.module.bn2.parameters()) + \
+#                   list(cnn_encoder.module.fc3.parameters()) + list(rnn_decoder.parameters())
 
-elif torch.cuda.device_count() == 1:
-    print("Using", torch.cuda.device_count(), "GPU!")
-    # Combine all EncoderCNN + DecoderRNN parameters
-    crnn_params = list(cnn_encoder.fc1.parameters()) + list(cnn_encoder.bn1.parameters()) + \
-                  list(cnn_encoder.fc2.parameters()) + list(cnn_encoder.bn2.parameters()) + \
-                  list(cnn_encoder.fc3.parameters()) + list(rnn_decoder.parameters())
+# elif torch.cuda.device_count() == 1:
+# print("Using", torch.cuda.device_count(), "GPU!")
+# Combine all EncoderCNN + DecoderRNN parameters
+crnn_params = list(cnn_encoder.fc1.parameters()) + list(cnn_encoder.bn1.parameters()) + \
+                list(cnn_encoder.fc2.parameters()) + list(cnn_encoder.bn2.parameters()) + \
+                list(cnn_encoder.fc3.parameters()) + list(rnn_decoder.parameters())
 
 optimizer = torch.optim.Adam(crnn_params, lr=learning_rate)
-
 
 # record training process
 epoch_train_losses = []
--- ResNetCRNN/functions.py
+++ ResNetCRNN/functions.py
@@ -1,4 +1,7 @@
 import os
+import cv2
+import json
+import glob
 import numpy as np
 from PIL import Image
 from torch.utils import data
@@ -22,88 +25,56 @@ def onehot2labels(label_encoder, y_onehot):
 def cat2labels(label_encoder, y_cat):
     return label_encoder.inverse_transform(y_cat).tolist()
 
-
-## ---------------------- Dataloaders ---------------------- ##
-# for 3DCNN
-class Dataset_3DCNN(data.Dataset):
-    "Characterizes a dataset for PyTorch"
-    def __init__(self, data_path, folders, labels, frames, transform=None):
-        "Initialization"
-        self.data_path = data_path
-        self.labels = labels
-        self.folders = folders
+class Dataset_CRNN(data.IterableDataset):
+    def __init__(self, path, seq_len=30, transform=None):
+        super(Dataset_CRNN).__init__()
+        self.path = path
+        self.video_files = list(map(os.path.basename, glob.glob(os.path.join(path, "*.mp4"))))
+        self.seq_len = seq_len
         self.transform = transform
-        self.frames = frames
 
-    def __len__(self):
-        "Denotes the total number of samples"
-        return len(self.folders)
+    def __iter__(self):
+        for filename in self.video_files:
+            print(filename)
+            cap = cv2.VideoCapture(os.path.join(self.path, filename))
+            filename_wo_ext = filename.rsplit('.', 1)[0]
 
-    def read_images(self, path, selected_folder, use_transform):
-        X = []
-        for i in self.frames:
-            image = Image.open(os.path.join(path, selected_folder, 'frame{:06d}.jpg'.format(i))).convert('L')
+            if cap.isOpened()== False: 
+                print("Error opening video stream or file")
+                continue
+            frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+            fps = int(cap.get(cv2.CAP_PROP_FPS))
 
-            if use_transform is not None:
-                image = use_transform(image)
+            gt = json.load(open(os.path.join(self.path, filename_wo_ext + ".funscript")))['actions']
+            positions = [int(x['pos'])/100 for x in gt]
+            frame_id = [int(x['at']) * fps // 1000 for x in gt]
+            positions_interp = np.interp(range(frame_count), frame_id, positions, left=0, right=0)
 
-            X.append(image.squeeze_(0))
-        X = torch.stack(X, dim=0)
+            frames = []
+            counter = 0
+            is_new_vid = True
+            while cap.isOpened():
+                if len(frames) == self.seq_len:
+                    i = counter - len(frames)
+                    j = counter
 
-        return X
+                    positions_frames = torch.Tensor(positions_interp[i: j])
+                    speed_frames = torch.Tensor(positions_interp[i: j] - positions_interp[i-1: j-1])
+                    yield torch.stack(frames, dim=0), torch.stack([positions_frames, speed_frames], dim=1), is_new_vid
+                    is_new_vid = False
+                    frames = []
+                ret, frame = cap.read()
+                if not ret:
+                    break
 
-    def __getitem__(self, index):
-        "Generates one sample of data"
-        # Select sample
-        folder = self.folders[index]
-
-        # Load data
-        X = self.read_images(self.data_path, folder, self.transform).unsqueeze_(0)  # (input) spatial images
-        y = torch.LongTensor([self.labels[index]])                             # (labels) LongTensor are for int64 instead of FloatTensor
-
-        # print(X.shape)
-        return X, y
-
-
-# for CRNN
-class Dataset_CRNN(data.Dataset):
-    "Characterizes a dataset for PyTorch"
-    def __init__(self, data_path, folders, labels, frames, transform=None):
-        "Initialization"
-        self.data_path = data_path
-        self.labels = labels
-        self.folders = folders
-        self.transform = transform
-        self.frames = frames
-
-    def __len__(self):
-        "Denotes the total number of samples"
-        return len(self.folders)
-
-    def read_images(self, path, selected_folder, use_transform):
-        X = []
-        for i in self.frames:
-            image = Image.open(os.path.join(path, selected_folder, 'frame{:06d}.jpg'.format(i)))
-
-            if use_transform is not None:
-                image = use_transform(image)
-
-            X.append(image)
-        X = torch.stack(X, dim=0)
-
-        return X
-
-    def __getitem__(self, index):
-        "Generates one sample of data"
-        # Select sample
-        folder = self.folders[index]
-
-        # Load data
-        X = self.read_images(self.data_path, folder, self.transform)     # (input) spatial images
-        y = torch.LongTensor([self.labels[index]])                  # (labels) LongTensor are for int64 instead of FloatTensor
-
-        # print(X.shape)
-        return X, y
+                if counter == 0:
+                    counter += 1
+                    continue
+                if self.transform:
+                    frame = self.transform(Image.fromarray(frame))
+                frames.append(frame)
+                counter += 1
+            cap.release()
 
 ## ---------------------- end of Dataloaders ---------------------- ##
 
@@ -142,74 +113,6 @@ def CRNN_final_prediction(model, device, loader):
     return all_y_pred
 
 ## -------------------- end of model prediction ---------------------- ##
-
-
-
-## ------------------------ 3D CNN module ---------------------- ##
-def conv3D_output_size(img_size, padding, kernel_size, stride):
-    # compute output shape of conv3D
-    outshape = (np.floor((img_size[0] + 2 * padding[0] - (kernel_size[0] - 1) - 1) / stride[0] + 1).astype(int),
-                np.floor((img_size[1] + 2 * padding[1] - (kernel_size[1] - 1) - 1) / stride[1] + 1).astype(int),
-                np.floor((img_size[2] + 2 * padding[2] - (kernel_size[2] - 1) - 1) / stride[2] + 1).astype(int))
-    return outshape
-
-class CNN3D(nn.Module):
-    def __init__(self, t_dim=120, img_x=90, img_y=120, drop_p=0.2, fc_hidden1=256, fc_hidden2=128, num_classes=50):
-        super(CNN3D, self).__init__()
-
-        # set video dimension
-        self.t_dim = t_dim
-        self.img_x = img_x
-        self.img_y = img_y
-        # fully connected layer hidden nodes
-        self.fc_hidden1, self.fc_hidden2 = fc_hidden1, fc_hidden2
-        self.drop_p = drop_p
-        self.num_classes = num_classes
-        self.ch1, self.ch2 = 32, 48
-        self.k1, self.k2 = (5, 5, 5), (3, 3, 3)  # 3d kernel size
-        self.s1, self.s2 = (2, 2, 2), (2, 2, 2)  # 3d strides
-        self.pd1, self.pd2 = (0, 0, 0), (0, 0, 0)  # 3d padding
-
-        # compute conv1 & conv2 output shape
-        self.conv1_outshape = conv3D_output_size((self.t_dim, self.img_x, self.img_y), self.pd1, self.k1, self.s1)
-        self.conv2_outshape = conv3D_output_size(self.conv1_outshape, self.pd2, self.k2, self.s2)
-
-        self.conv1 = nn.Conv3d(in_channels=1, out_channels=self.ch1, kernel_size=self.k1, stride=self.s1,
-                               padding=self.pd1)
-        self.bn1 = nn.BatchNorm3d(self.ch1)
-        self.conv2 = nn.Conv3d(in_channels=self.ch1, out_channels=self.ch2, kernel_size=self.k2, stride=self.s2,
-                               padding=self.pd2)
-        self.bn2 = nn.BatchNorm3d(self.ch2)
-        self.relu = nn.ReLU(inplace=True)
-        self.drop = nn.Dropout3d(self.drop_p)
-        self.pool = nn.MaxPool3d(2)
-        self.fc1 = nn.Linear(self.ch2 * self.conv2_outshape[0] * self.conv2_outshape[1] * self.conv2_outshape[2],
-                             self.fc_hidden1)  # fully connected hidden layer
-        self.fc2 = nn.Linear(self.fc_hidden1, self.fc_hidden2)
-        self.fc3 = nn.Linear(self.fc_hidden2, self.num_classes)  # fully connected layer, output = multi-classes
-
-    def forward(self, x_3d):
-        # Conv 1
-        x = self.conv1(x_3d)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.drop(x)
-        # Conv 2
-        x = self.conv2(x)
-        x = self.bn2(x)
-        x = self.relu(x)
-        x = self.drop(x)
-        # FC 1 and 2
-        x = x.view(x.size(0), -1)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.dropout(x, p=self.drop_p, training=self.training)
-        x = self.fc3(x)
-
-        return x
-
-## --------------------- end of 3D CNN module ---------------- ##
-
 
 
 ## ------------------------ CRNN module ---------------------- ##
@@ -313,7 +216,7 @@ class ResCNNEncoder(nn.Module):
         self.fc_hidden1, self.fc_hidden2 = fc_hidden1, fc_hidden2
         self.drop_p = drop_p
 
-        resnet = models.resnet152(pretrained=True)
+        resnet = models.resnet50(pretrained=True)
         modules = list(resnet.children())[:-1]      # delete the last fc layer.
         self.resnet = nn.Sequential(*modules)
         self.fc1 = nn.Linear(resnet.fc.in_features, fc_hidden1)
@@ -322,33 +225,33 @@ class ResCNNEncoder(nn.Module):
         self.bn2 = nn.BatchNorm1d(fc_hidden2, momentum=0.01)
         self.fc3 = nn.Linear(fc_hidden2, CNN_embed_dim)
         
-    def forward(self, x_3d):
+    def forward(self, x_seq):
         cnn_embed_seq = []
-        for t in range(x_3d.size(1)):
+        # for t in range(x_3d.size(1)):
             # ResNet CNN
-            with torch.no_grad():
-                x = self.resnet(x_3d[:, t, :, :, :])  # ResNet
-                x = x.view(x.size(0), -1)             # flatten output of conv
+        with torch.no_grad():
+            x = self.resnet(x_seq)  # ResNet
+            x = x.view(x.shape[0], -1)             # flatten output of conv
 
-            # FC layers
-            x = self.bn1(self.fc1(x))
-            x = F.relu(x)
-            x = self.bn2(self.fc2(x))
-            x = F.relu(x)
-            x = F.dropout(x, p=self.drop_p, training=self.training)
-            x = self.fc3(x)
+        # FC layers
+        x = self.bn1(self.fc1(x))
+        x = F.relu(x)
+        x = self.bn2(self.fc2(x))
+        x = F.relu(x)
+        x = F.dropout(x, p=self.drop_p, training=self.training)
+        x = self.fc3(x)
 
-            cnn_embed_seq.append(x)
+        cnn_embed_seq = x.unsqueeze(1)
+        # cnn_embed_seq.append(x)
 
         # swap time and sample dim such that (sample dim, time dim, CNN latent dim)
-        cnn_embed_seq = torch.stack(cnn_embed_seq, dim=0).transpose_(0, 1)
+        # cnn_embed_seq = torch.stack(cnn_embed_seq, dim=0).transpose_(0, 1)
         # cnn_embed_seq: shape=(batch, time_step, input_size)
-
         return cnn_embed_seq
 
 
 class DecoderRNN(nn.Module):
-    def __init__(self, CNN_embed_dim=300, h_RNN_layers=3, h_RNN=256, h_FC_dim=128, drop_p=0.3, num_classes=50):
+    def __init__(self, CNN_embed_dim=300, h_RNN_layers=3, h_RNN=256, h_FC_dim=128, drop_p=0.3):
         super(DecoderRNN, self).__init__()
 
         self.RNN_input_size = CNN_embed_dim
@@ -356,7 +259,6 @@ class DecoderRNN(nn.Module):
         self.h_RNN = h_RNN                 # RNN hidden nodes
         self.h_FC_dim = h_FC_dim
         self.drop_p = drop_p
-        self.num_classes = num_classes
 
         self.LSTM = nn.LSTM(
             input_size=self.RNN_input_size,
@@ -366,22 +268,22 @@ class DecoderRNN(nn.Module):
         )
 
         self.fc1 = nn.Linear(self.h_RNN, self.h_FC_dim)
-        self.fc2 = nn.Linear(self.h_FC_dim, self.num_classes)
+        self.fc2 = nn.Linear(self.h_FC_dim, 1)
 
-    def forward(self, x_RNN):
+    def forward(self, x_RNN, h_0=None, c_0=None):
         
         self.LSTM.flatten_parameters()
-        RNN_out, (h_n, h_c) = self.LSTM(x_RNN, None)  
-        """ h_n shape (n_layers, batch, hidden_size), h_c shape (n_layers, batch, hidden_size) """ 
-        """ None represents zero initial hidden state. RNN_out has shape=(batch, time_step, output_size) """
+        RNN_out, (h_n, c_n) = self.LSTM(x_RNN, None if h_0 is None or c_0 is None else (h_0, c_0))
+        """ h_n shape (n_layers, batch, hidden_size), c_n shape (n_layers, batch, hidden_size) """ 
+        """ None represents zero initial hidden state. RNN_out has shape=(seq_len, batch, num_directions * hidden_size) """
 
         # FC layers
-        x = self.fc1(RNN_out[:, -1, :])   # choose RNN_out at the last time step
+        x = self.fc1(RNN_out)
         x = F.relu(x)
         x = F.dropout(x, p=self.drop_p, training=self.training)
         x = self.fc2(x)
 
-        return x
+        return x, h_n.detach(), c_n.detach()
 
 ## ---------------------- end of CRNN module ---------------------- ##
 
